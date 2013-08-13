b9d1c27fe3144e137117ddf363007ab6c4030b05
Anna <myearwen@gmail.com>
2013-08-13 12:08:58 +0400

    Clean up of the NeuralNetworkDistributionStrategy.

--- library/src/main/java/net/sourceforge/cilib/coevolution/cooperative/problemdistribution/NeuralNetworkDistributionStrategy.java
+++ library/src/main/java/net/sourceforge/cilib/coevolution/cooperative/problemdistribution/NeuralNetworkDistributionStrategy.java
@@ -24,7 +24,6 @@ package net.sourceforge.cilib.coevolution.cooperative.problemdistribution;
 import com.google.common.base.Preconditions;
 import java.util.List;
 
-import net.sourceforge.cilib.algorithm.Algorithm;
 import net.sourceforge.cilib.algorithm.population.PopulationBasedAlgorithm;
 import net.sourceforge.cilib.coevolution.cooperative.CooperativeCoevolutionAlgorithm;
 import net.sourceforge.cilib.coevolution.cooperative.problem.CooperativeCoevolutionProblemAdapter;
@@ -43,7 +42,7 @@ import net.sourceforge.cilib.type.types.container.Vector;
 public class NeuralNetworkDistributionStrategy implements ProblemDistributionStrategy {
 
     /**
-     * Splits up the given {@link OptimisationProblem} into sub-problems, where each sub problem contains a sequential, uniform length, portion of the problem verctor, and assigns all the sub-problems to the sub population {@link Algorithm}s.
+     * Splits up the given {@link OptimisationProblem} into sub-problems, where each sub problem corresponds to a single layer of NN weights.
      * @param populations The list of participating {@linkplain PopulationBasedAlgorithm}s.
      * @param problem The problem that needs to be re-distributed.
      * @param context The context vector maintained by the {@linkplain CooperativeCoevolutionAlgorithm}.
@@ -55,9 +54,8 @@ public class NeuralNetworkDistributionStrategy implements ProblemDistributionStr
         NNTrainingProblem problem = (NNTrainingProblem)p; // convert optimisation problem to NNTrainingProblem
         NeuralNetwork nn = problem.getNeuralNetwork();
         int numLayers = nn.getArchitecture().getNumLayers();
-        //System.out.println("NumLayers: " + numLayers + ", populations.size = " + populations.size());
         Preconditions.checkArgument(numLayers - 1 == populations.size(), "The number of populations should be equal to the number of NN layers minus one");
-        //System.out.println("NumWeights: " + nn.getWeights().size());
+
         int offset = 0;
         for(int l = 1; l < nn.getArchitecture().getNumLayers(); l++) { // go through the NN layer by layer, assigning weights to subpopulations
             int previousLayerSize = nn.getArchitecture().getLayers().get(l - 1).size();
@@ -66,7 +64,6 @@ public class NeuralNetworkDistributionStrategy implements ProblemDistributionStr
                 thisLayerSize -=1;
             } // subtract bias, unless we're dealing with output layers
             int subpopulationSize = previousLayerSize * thisLayerSize;
-            //System.out.println("l = " + l + ", previousLayerSize = " + previousLayerSize + ", thisLayerSize = "+thisLayerSize);
             DimensionAllocation allocation = new SequencialDimensionAllocation(offset, subpopulationSize);
             populations.get(l - 1).setOptimisationProblem(new CooperativeCoevolutionProblemAdapter(problem, allocation, context));
             offset += subpopulationSize;
