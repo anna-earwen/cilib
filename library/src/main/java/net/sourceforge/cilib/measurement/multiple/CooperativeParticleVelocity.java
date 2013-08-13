f9350ebbe7342eb909f46ad4a966ac0a11b1a127
Anna <myearwen@gmail.com>
2013-08-13 14:47:37 +0400

    A bunch of measurements added.

---
+++ library/src/main/java/net/sourceforge/cilib/measurement/multiple/CooperativeParticleVelocity.java
@@ -0,0 +1,53 @@
+/**           __  __
+ *    _____ _/ /_/ /_    Computational Intelligence Library (CIlib)
+ *   / ___/ / / / __ \   (c) CIRG @ UP
+ *  / /__/ / / / /_/ /   http://cilib.net
+ *  \___/_/_/_/_.___/
+ */
+package net.sourceforge.cilib.measurement.multiple;
+
+import net.sourceforge.cilib.algorithm.Algorithm;
+import net.sourceforge.cilib.algorithm.population.MultiPopulationBasedAlgorithm;
+import net.sourceforge.cilib.algorithm.population.PopulationBasedAlgorithm;
+import net.sourceforge.cilib.entity.Entity;
+import net.sourceforge.cilib.entity.EntityType;
+import net.sourceforge.cilib.measurement.Measurement;
+import net.sourceforge.cilib.problem.solution.Fitness;
+import net.sourceforge.cilib.type.types.Numeric;
+import net.sourceforge.cilib.type.types.container.Vector;
+import net.sourceforge.cilib.type.types.container.Vector.Builder;
+
+/**
+ * This measurement only works for PSO. Simply find the Entity with the Best person best fitness in the population without re-calculating the fitness.
+ */
+public class CooperativeParticleVelocity implements Measurement {
+
+    private static final long serialVersionUID = 6502384299554109943L;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CooperativeParticleVelocity getClone() {
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Vector getValue(Algorithm algorithm) {
+        Builder builder = Vector.newBuilder(); 
+        MultiPopulationBasedAlgorithm ca = (MultiPopulationBasedAlgorithm) algorithm;
+        for (PopulationBasedAlgorithm currentAlgorithm : ca.getPopulations()) {
+            for (Entity e : currentAlgorithm.getTopology()) {
+                Vector velocity = (Vector)e.getProperties().get(EntityType.Particle.VELOCITY);
+                for(Numeric n : velocity) {
+                    builder.add(n);
+                }
+            }
+        }
+        return builder.build();
+    }
+    
+}
---
+++ library/src/main/java/net/sourceforge/cilib/measurement/single/AbsoluteAverageVelocity.java
@@ -0,0 +1,91 @@
+/**           __  __
+ *    _____ _/ /_/ /_    Computational Intelligence Library (CIlib)
+ *   / ___/ / / / __ \   (c) CIRG @ UP
+ *  / /__/ / / / /_/ /   http://cilib.net
+ *  \___/_/_/_/_.___/
+ */
+package net.sourceforge.cilib.measurement.single;
+
+import net.sourceforge.cilib.measurement.multiple.*;
+import net.sourceforge.cilib.algorithm.Algorithm;
+import net.sourceforge.cilib.algorithm.population.MultiPopulationBasedAlgorithm;
+import net.sourceforge.cilib.algorithm.population.PopulationBasedAlgorithm;
+import net.sourceforge.cilib.entity.Entity;
+import net.sourceforge.cilib.entity.EntityType;
+import net.sourceforge.cilib.measurement.Measurement;
+import net.sourceforge.cilib.type.types.Numeric;
+import net.sourceforge.cilib.type.types.Real;
+import net.sourceforge.cilib.type.types.container.Vector;
+import net.sourceforge.cilib.type.types.container.Vector.Builder;
+
+/**
+ * This measurement only works for PSO. Simply find the Entity with the Best person best fitness in the population without re-calculating the fitness.
+ */
+public class AbsoluteAverageVelocity implements Measurement {
+
+    private static final long serialVersionUID = 6502384299554109943L;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public AbsoluteAverageVelocity getClone() {
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Vector getValue(Algorithm algorithm) {
+        Builder builder = Vector.newBuilder(); 
+        PopulationBasedAlgorithm currentAlgorithm = (PopulationBasedAlgorithm) algorithm;
+        int dimension = currentAlgorithm.getTopology().get(0).getDimension();
+        Vector averageVelocity = Vector.of();//new Numeric[dimension]);
+        for (Entity e : currentAlgorithm.getTopology()) {
+            Vector velocity = (Vector)e.getProperties().get(EntityType.Particle.VELOCITY);
+            for(int i = 0; i < dimension; i++) {
+                if(averageVelocity.size() < dimension) {
+                    averageVelocity.add(velocity.get(i));
+                } else {
+                averageVelocity.set(i, Real.valueOf(averageVelocity.doubleValueOf(i) + velocity.doubleValueOf(i)));
+                }
+            }
+        }
+        for(int i = 0; i < dimension; i++) {
+            averageVelocity.set(i, Real.valueOf(Math.abs(averageVelocity.doubleValueOf(i)/(double)dimension)));
+        }
+        builder.copyOf(averageVelocity);
+        
+        return builder.build();
+        
+        /*
+         * PopulationBasedAlgorithm populationBasedAlgorithm = (PopulationBasedAlgorithm) algorithm;
+        int populationSize = populationBasedAlgorithm.getTopology().size();
+
+        int dimensions = 0;
+        double sumOfAverageConvergedDimensions = 0.0;
+
+        for (Entity populationEntity : populationBasedAlgorithm.getTopology()) {
+            dimensions = populationEntity.getDimension();
+
+            int dimension = 0;
+            int numberConvergedDimensions = 0;
+            for (Numeric position : (Vector) populationEntity.getCandidateSolution()) {
+                double lowerBound = targetSolution.doubleValueOf(dimension) - this.errorThreshold.getParameter();
+                double upperBound = targetSolution.doubleValueOf(dimension) + this.errorThreshold.getParameter();
+                double value = position.doubleValue();
+
+                if ((value >= lowerBound) && (value <= upperBound)) {
+                    numberConvergedDimensions++;
+                }
+                dimension++;
+            }
+            sumOfAverageConvergedDimensions += (double) numberConvergedDimensions / (double) dimensions;
+        }
+
+        return Real.valueOf(sumOfAverageConvergedDimensions / (double) populationSize * (double) dimensions);
+         */
+    }
+    
+}
